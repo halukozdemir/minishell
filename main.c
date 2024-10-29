13f1747f7cf0fd1dad22fe5df0bdadf0bea3a533
halukozdemir <0halukozdemir@gmail.com>
2024-10-29 16:15:11 +0300

    remove builtsin

--- main.c
+++ main.c
@@ -4,47 +4,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-// Function to check and execute built-in commands
-int execute_builtin(char **args, char ***new_env)
-{
-    if (ft_strncmp(args[0], "echo", 4) == 0)
-    {
-        ft_echo(args);
-        return 1;
-    }
-    else if (ft_strncmp(args[0], "cd", 2) == 0)
-    {
-        ft_cd(args, *new_env);
-        return 1;
-    }
-    else if (ft_strncmp(args[0], "pwd", 3) == 0)
-    {
-        ft_pwd();
-        return 1;
-    }
-    else if (ft_strncmp(args[0], "export", 6) == 0)
-    {
-        ft_export(args, new_env);
-        return 1;
-    }
-    else if (ft_strncmp(args[0], "unset", 5) == 0)
-    {
-        ft_unset(args, new_env);
-        return 1;
-    }
-    else if (ft_strncmp(args[0], "env", 3) == 0)
-    {
-        ft_env(*new_env);
-        return 1;
-    }
-    else if (ft_strncmp(args[0], "exit", 4) == 0)
-    {
-        ft_exit();
-        return 1;
-    }
-    return 0;
-}
-
 int main(int argc, char **argv, char **env)
 {
     char    *input;
