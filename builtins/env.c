9966ef08599aef944800b709d28709f50de753c4
halukozdemir <0halukozdemir@gmail.com>
2024-11-11 19:58:56 +0300

    yapılacaklar düzenlendi

--- builtins/env.c
+++ builtins/env.c
@@ -5,11 +5,8 @@ void	env(t_env *env)
 	t_env *env_list;
 
 	env_list = env;
-    int i = 0;
     while (env_list)
     {
-        i++;
-        printf("i : %d\n", i);
         write(1, env_list->key, ft_strlen(env_list->key));
         write(1, "=", 1);
         write(1, env_list->value, ft_strlen(env_list->value));
