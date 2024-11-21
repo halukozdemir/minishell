377d8d58dbf1e9317baa1805c469d8a435c8032c
Haluk Mert Ozdemir <halozdem@k1m24s04.42istanbul.com.tr>
2024-11-21 21:22:30 +0300

    finitttooo kanzilettttooooooooo

--- parser/shell_utils.c
+++ parser/shell_utils.c
@@ -6,7 +6,7 @@
 /*   By: halozdem <halozdem@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 18:52:24 by halozdem          #+#    #+#             */
-/*   Updated: 2024/11/21 17:32:48 by halozdem         ###   ########.fr       */
+/*   Updated: 2024/11/21 21:19:12 by halozdem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,22 +25,26 @@ void	initialize_shell(t_mshell *mshell, char **env, t_env **env_list)
 void	run_shell(t_mshell *mshell)
 {
 	char	*input;
+	char	*input2;
 
 	while (1)
 	{
 		reset_job_list(mshell);
 		set_signal(MAIN_SF);
 		input = readline("minishell> ");
-		input = ft_strtrim(input, " \t\r\v\f");
-		if (!input)
+		input2 = ft_strtrim(input, " \t\r\v\f");
+		free(input);
+		if (!input2)
 		{
-			printf("sdfs\n");
-			free(input);
+			free(input2);
 			break ;
 		}
 		set_signal(3);
-		if (process_user_input(input, mshell) == EXIT_FAILURE)
+		if (process_user_input(input2, mshell) == EXIT_FAILURE)
+		{
+			free(input2);
 			continue ;
+		}
 		executor(mshell);
 	}
 }
